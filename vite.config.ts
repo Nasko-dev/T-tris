81a57a62df9f30d7a38fb8f1df2d128ba4a68f28
William-Dev <williamlegall5@gmail.com>
2025-02-02 01:35:02 +0100

    Fix beug

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/T-tris/", // Doit correspondre exactement au nom de votre dépôt
   plugins: [react()],
+  base: "/T-tris/", // Doit correspondre exactement au nom de votre dépôt
 });
